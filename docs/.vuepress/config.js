03c363ffc416fb00ee94790d216e5b4e1d71ea74
KamenRider41 <229335832@qq.com>
2022-05-05 09:12:27 +0800

    feat

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,12 @@
  * @Author: 41
  * @Date: 2022-05-05 08:24:23
  * @LastEditors: 41
- * @LastEditTime: 2022-05-05 08:30:57
+ * @LastEditTime: 2022-05-05 09:05:01
  * @Description: 
  */
 // .vuepress/config.js
 module.exports = {
+  base: '/KamenUI-docs/',
   themeConfig: {
     sidebar: [
       '/',
