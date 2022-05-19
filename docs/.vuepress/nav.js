4ae54205f4aa62fbdb72793023b7a0aa82e9223d
KamenRider41 <229335832@qq.com>
2022-05-19 21:08:42 +0800

    fix(nav-aboutme)

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -3,7 +3,7 @@ module.exports = [{
 }, {
   text: '关于我',
   items: [
-    { text: '关于我', link: '/partDocs/aboutme' },
+    { text: '关于我', link: '/componentDocs/aboutme' },
     { text: 'Github', link: 'https://github.com/KamenRider41/Hundred-refining-into-Immortals' },
     { text: 'CSDN', link: 'https://blog.csdn.net/qq_42136832' },
     { text: '掘金', link: 'https://juejin.cn/user/1108753576635751/posts' },
